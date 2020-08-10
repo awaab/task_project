8229b2be1174cdd4e21e375acc1d413c916de91a
awaab <awaaab10@gmail.com>
2020-08-10 23:13:37 +0400

    prod secure settings

--- task_project/settings_prod.py
+++ task_project/settings_prod.py
@@ -5,3 +5,9 @@ ALLOWED_HOSTS = ['django-assess-task.herokuapp.com']
 DEBUG = False
 
 ADMIN_ENABLED = False
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
