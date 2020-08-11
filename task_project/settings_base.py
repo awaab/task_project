b86ef78bc00560ff94c505f4443c9fe289b72f52
awaab <awaaab10@gmail.com>
2020-08-11 10:36:01 +0400

    static files

--- task_project/settings_base.py
+++ task_project/settings_base.py
@@ -125,8 +125,8 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'front_app', "build", 'static'),
-    os.path.join(BASE_DIR, 'front_app', "build"),
+    os.path.join(BASE_DIR, 'front_app', "build", 'static'),
+    # os.path.join(BASE_DIR, 'front_app', "build"),
     # update the STATICFILES_DIRS
 )
 
